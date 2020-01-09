13000b8058896dbcca78762676b4c146efe50859
haresh0610 <haresh.samnani@gmail.com>
2020-01-09 17:43:27 +0530

    Added my react app

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="http://sublimedatasystems.in">
+          Tic-Tac-Toe
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
